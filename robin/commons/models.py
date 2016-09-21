67fafd73057ca70ede94ddeb618d819a477c1c69
hereischen <hachen@redhat.com>
2016-09-21 17:13:39 +0800

    commons models done

--- robin/commons/models.py
+++ robin/commons/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+# create and modified date for every item in the database.
+class Timestampable(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='create date')
+    modified_date = models.DateTimeField(auto_now=True, verbose_name='modified date')
+
+    class Meta:
+        abstract = True
