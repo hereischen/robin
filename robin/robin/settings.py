5ded5a22d559e2c6bc8083df4cfb51a50b929d1e
hereischen <hachen@redhat.com>
2016-09-21 13:34:12 +0800

    adding 3 new app

--- robin/robin/settings.py
+++ robin/robin/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commons',
+    'members',
+    'statistics',
 ]
 
 MIDDLEWARE_CLASSES = [
