cce686703b4067616eda6f997ff75dcdff0d8864
hereischen <hachen@redhat.com>
2016-09-26 12:50:23 +0800

    added get_issues for get all issues, and get_a_page_of_issues

--- robin/scripts/githublight.py
+++ robin/scripts/githublight.py
@@ -20,9 +20,26 @@ class Repository(object):
         r = requests.get(url, params=params)
         return r.json()
 
-    def get_issues(self, page=1, since=None, access_token=None):
+    def get_issues(self, since=None, page=1, state='all', access_token=None):
         url = '/'.join([self.url, 'issues'])
-        params = {'since': since, 'page': page, 'access_token': access_token}
+        # based on observations, default items returned by guthub api v3 is 30
+        per_page = 30
+        res = []
+        while True:
+            # print 'page number >>>', page
+            params = {'since': since, 'page': page, 'state': state, 'access_token': access_token}
+            r = requests.get(url, params)
+            res.extend(r.json())
+            page += 1
+            # print 'len of this page', len(r.json())
+            if len(r.json()) < per_page:
+                break
+        # print len(res)
+        return res
+
+    def get_a_page_of_issues(self, page=1, since=None, state='all', access_token=None):
+        url = '/'.join([self.url, 'issues'])
+        params = {'since': since, 'page': page, 'state': state, 'access_token': access_token}
         r = requests.get(url, params)
         return r.json()
 
@@ -53,7 +70,8 @@ class Repository(object):
 
 # *************************************************************************
 # print Repository('hereischen', 'robin').get_commits_by_email(email='hachen@redhat.com', until='2016-09-16T05:50:35Z')
-# print Repository('avocado-framework', 'avocado-vt').get_issues()
+print Repository('avocado-framework', 'avocado-vt').get_issues()
+# print Repository('avocado-framework', 'avocado-vt').get_a_page_of_issues(page=2)
 # print Repository('avocado-framework', 'avocado-vt').get_issue_by_number('709')
 # print len(Repository('avocado-framework', 'avocado-vt').get_issue_comments('557'))
 # print Repository('avocado-framework', 'avocado-vt').get_pull_by_number('709')
