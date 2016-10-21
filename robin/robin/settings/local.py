439fcebe3f3bc720b8667950883ed804ff622ea7
hereischen <hachen@redhat.com>
2016-10-21 13:16:22 +0800

    hide access token in loacl

--- robin/robin/settings/local.py
+++ robin/robin/settings/local.py
@@ -4,6 +4,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+KEY_ROOT = '/Users/hereischen/keys/'
+
 SERVER_ALIAS = 'local'
 
 CRONTAB_DJANGO_SETTINGS_MODULE = 'robin.settings.local'
+
+with open(KEY_ROOT + 'access_token.txt') as f:
+    ACCESS_TOKEN = f.read().strip()
