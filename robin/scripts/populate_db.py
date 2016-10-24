c3e362ee859d615e52ef67390e65871bba751b3a
hereischen <hachen@redhat.com>
2016-10-24 18:28:50 +0800

    new populate_db scripts

--- robin/scripts/populate_db.py
+++ robin/scripts/populate_db.py
@@ -1,31 +1,41 @@
+import os
+import csv
 from members.models import Member, Team
 from statistics.models import Repository
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print os.path.join(ROOT_DIR, 'team_member_data.csv')
+
 
 def populate_members():
-    team = Team.objects.get_or_create(team_code='x86', team_name='kvm_autotest')[0]
-    print team
-    Member.objects.create(kerbroes_id='hachen',
-                          name='Haotong Chen',
-                          github_account='hereischen',
-                          team=team)
-    Member.objects.create(kerbroes_id='xutian',
-                          name='Xu Tian',
-                          github_account='xutian',
-                          team=team)
-    Member.objects.create(kerbroes_id='coli',
-                          name='Cong Li',
-                          github_account='CongLi',
-                          team=team)
+    team = Team.objects.get_or_create(team_code='kvm', team_name='KVM QE')[0]
+    print 'created team : %s' % team
 
-    print 'populate_members'
+    with open(os.path.join(ROOT_DIR, 'team_member_data.csv')) as csvfile:
+        reader = csv.DictReader(csvfile)
+        counter = 0
+        for row in reader:
+            if row['Github_Account'] != '':
+                counter += 1
+                print(row['Github_Account'], row['Name'], row['Kerberos'])
+                member = Member.objects.create(kerbroes_id=row['Kerberos'],
+                                               name=row['Name'],
+                                               github_account=row['Github_Account'],
+                                               team=team)
+                print member
+        print counter
+    print 'populate_members done'
 
 
 def populate_repos():
-    Repository.objects.create(owner='hereischen', repo='robin')
     Repository.objects.create(owner='avocado-framework', repo='avocado-vt')
+    Repository.objects.create(owner='avocado-framework', repo='avocado')
+    Repository.objects.create(owner='autotest', repo='tp-qemu')
+    Repository.objects.create(owner='autotest', repo='autotest-client-tests')
+    repos = Repository.objects.all()
 
-    print 'populate_repos'
+    print '%s repos are created' % len(repos)
+    print repos
 
 
 def populate_db():
