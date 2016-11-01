8f98faa97f2bb40a2e00ece103ee4a31354600d1
hereischen <hachen@redhat.com>
2016-11-01 18:36:56 +0800

    using relative dirpath in setting/local.py

--- robin/robin/settings/local.py
+++ robin/robin/settings/local.py
@@ -1,14 +1,16 @@
+import os
 from .base import *
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-KEY_ROOT = '/Users/hereischen/keys/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+KEY_ROOT = os.path.join(BASE_DIR, 'keys')
 
 SERVER_ALIAS = 'local'
 
 CRONTAB_DJANGO_SETTINGS_MODULE = 'robin.settings.local'
 
-with open(KEY_ROOT + 'access_token.txt') as f:
+with open(os.path.join(KEY_ROOT, 'access_token.txt')) as f:
     ACCESS_TOKEN = f.read().strip()
