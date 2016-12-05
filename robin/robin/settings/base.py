afbfa43b1b3c3f22fbe378f1d48935b48db57a6e
Haotong Chen <hachen@redhat.com>
2016-12-05 15:40:12 +0800

    update settings, enable 2000 items per page

--- robin/robin/settings/base.py
+++ robin/robin/settings/base.py
@@ -216,7 +216,7 @@ CRONJOBS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'robin.api.core.pagination.CustomPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 2000,
 }
 
 # Github access_token. API rate limit,
