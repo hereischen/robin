02911bfa2754470751a5380e2576c4e884d27270
Haotong Chen <hachen@redhat.com>
2017-01-06 18:49:01 +0800

    changed summaries models fields

--- robin/summaries/models.py
+++ robin/summaries/models.py
@@ -1,8 +1,18 @@
 from __future__ import unicode_literals
 
+from commons.models import Timestampable
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from commons.models import Timestampable
+
+
+# summary type
+SUMMARY_TYPE = (
+    (0, 'REVIEWS'),
+    (1, 'SUBMITTED PATCHES'),
+    (2, 'UPDATED PATCHES'),
+    (3, 'MERGED PATCHES'),
+)
 
 
 # Create your models here.
@@ -12,14 +22,18 @@ class Weekly(Timestampable, models.Model):
     """
     from_date = models.DateTimeField(verbose_name='from date')
     to_date = models.DateTimeField(verbose_name='to_date')
-    week_number = models.CharField(max_length=16, unique=True, verbose_name='week bumber')
-    summary_type = models.CharField(max_length=16,verbose_name='summary type')
-    number_of_pulls = models.PostiveIntegerField(default=0, verbose_name='number of pulls')
+    # week number eg. 2017w1
+    week_number = models.CharField(max_length=16, unique=True,
+                                   verbose_name='week number')
+    summary_type = models.PositiveSmallIntegerField(default=1,
+                                                    choices=SUMMARY_TYPE,
+                                                    verbose_name='summary type')
+    quantity = models.IntegerField(default=0,
+                                   verbose_name='quantity of items')
 
     class Meta:
         verbose_name = _('weekly_summary')
-        verbose_name_pluarl = _('weekly_summaries')
+        verbose_name_plural = _('weekly_summaries')
 
     def __unicode__(self):
         return str(self.week_number)
-
